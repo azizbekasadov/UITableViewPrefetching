cfe66019a6661fe838af5c2d9317c5511ecf6f92
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-22 11:49:20 +0500

    Initial Commit

---
+++ UITableViewPrefetching/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewPrefetching
+//
+//  Created by Azizbek Asadov on 22/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
